472ba2a1b040ea50a8101bf843e04d09025586f3
Unknown <mawuli.adjei@ashesi.edu.gh>
2018-06-17 15:17:37 +0000

    bottom padding on images fixed. -- will you be my paddy?

--- js/photos.js
+++ js/photos.js
@@ -8,7 +8,7 @@ $.ajax({
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
             if( val.match(/\.(jpg|png|gif)$/) ) {
-                $("#photos").append( "<div class='col-6 col-lg-3'><a href='#'><img alt='100%x180' src='"+ folder + val +"' class='img-thumbnail' style='height: 100%; width: 100%; display: block;'></a></div>" );
+                $("#photos").append( "<div class='col-6 col-lg-3'><a href='#'><img alt='100%x180' src='"+ folder + val +"' class='img-thumbnail' style='padding-bottom: 30px''height: 100%; width: 100%; display: block;'></a></div>" );
 
             }
         });
