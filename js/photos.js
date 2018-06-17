c2f164b4d7a6f58e9bb5bf75839ac5d344a0a11a
Unknown <mawuli.adjei@ashesi.edu.gh>
2018-06-17 17:48:28 +0000

    I can see blurrrrry now the rain is gone

--- js/photos.js
+++ js/photos.js
@@ -8,7 +8,7 @@ $.ajax({
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
             if( val.match(/\.(jpg|png|gif)$/) ) {
-                $("#photos").append( "<div class='col-6 col-lg-3'><a href='#'><img alt='100%x180' src='"+ folder + val +"' class='img-thumbnail' style='padding-bottom: 30px''height: 100%; width: 100%; display: block;'></a></div>" );
+                $("#photos").append( "<div class='col-6 col-lg-3'><a href='#'><img alt='100%x180' src='"+ folder + val +"' class='img-thumbnail' style='padding-bottom: 30px; height: 100%; width: 100%; display: block;'></a></div>" );
 
             }
         });
@@ -20,11 +20,13 @@ $.ajax({
 //funtion for pin/key modal
 (function() {
   document.getElementById('fix').style.overflow= "hidden";
+  document.getElementById('blur').style.filter= "blur(5px)";
 var o = document.getElementById('modal-key');
 o.getElementsByTagName('form')[0].onsubmit = function() {
     if (this.answer.value === 'yrbk2018') {
         o.style.display = "none";
         document.getElementById('fix').style.overflow= "auto";
+        document.getElementById('blur').style.filter= "blur(0px)";
     } else {
       $("#pin").css("animation","shake 0.5s");
     }
