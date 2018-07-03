99498fd9463b66bcc0c2adea9d06de87431d22da
Maame Yaa Afriyie Poku <maameyaa@techloftgh.com>
2018-07-03 09:13:33 +0000

    sorry about my commit-ment issuus
    a whole lot of changes

--- public/js/photos.js
+++ public/js/photos.js
@@ -1,79 +1,42 @@
 // funtion to display all the photos in a grid
 
 $(document).ready(function(){
-        var folder = "img/schools/ashesi/classes/2018/";
+  var folder = "img/schools/ashesi/classes/2018/";
 
-        for(i=1; i<5; i++ ){
+  console.log(folder);
 
-          $("#photos").append( "<div class='col-6 col-lg-3' style='margin-bottom: 30px;'><a data-lightbox='image' href='"+ folder + i + '.jpg'+"' ><img alt='100%x180' src='"+ folder + i + '.jpg'+ "' class='img-thumbnail' style='height: 100%; width: 100%; display: block;'></a></div>" );
-        }
-// $.ajax({
-//     url : folder,
-//     success: function (data) {
-//
-//       console.log(data);
-//         // $(data).find("a").attr("href", function (i, val) {
-//         //     if( val.match(/\.(jpg|png|gif)$/) ) {
-//         //         $("#photos").append( "<div class='col-6 col-lg-3' style='margin-bottom: 30px;'><a data-lightbox='image' href='"+ folder + val +"' ><img alt='100%x180' src='"+ folder + val +"' class='img-thumbnail' style='height: 100%; width: 100%; display: block;'></a></div>" );
-//         //     }
-//         // });
-//     }
-// });
+  for(i=1; i<=8; i++ ){
+    // console.log(i);
+
+    $("#photos").append( "<div class='col-6 col-lg-3' style='margin-bottom: 30px;'><a onclick='getimage(i)' data-toggle='modal' data-target='#myModal'><img alt='100%x180' src='"+ folder + i + '.jpg'+ "' class='img-thumbnail' style='height: 100%; width: 100%; display: block;'></a></div>"  );
+  }
+
+  function getimage(x){
+
+    console.log(x);
+    var folder = "img/schools/ashesi/classes/2018/";
+
+    $("#photos").append( "<div class='modal fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'><div class='modal-dialog'><div class='modal-body'><div class='modal-content'><div class='modal-body'><h4>Text in a modal</h4><img alt='100%x180' src='"+ folder + x + '.jpg'+ "' class='img-thumbnail' style='height: 100%; width: 100%; display: block;'></div></div></div></div></div>");
+  }
 });
+
+
+
 
 
 //funtion for pin/key modal
 (function() {
   document.getElementById('fix').style.overflow= "hidden";
   document.getElementById('blur').style.filter= "blur(5px)";
-var o = document.getElementById('modal-key');
-o.getElementsByTagName('form')[0].onsubmit = function() {
+  var o = document.getElementById('modal-key');
+  o.getElementsByTagName('form')[0].onsubmit = function() {
     if (this.answer.value === 'yrbk2018') {
-        o.style.display = "none";
-        document.getElementById('fix').style.overflow= "auto";
-        document.getElementById('blur').style.filter= "blur(0px)";
+      o.style.display = "none";
+      document.getElementById('fix').style.overflow= "auto";
+      document.getElementById('blur').style.filter= "blur(0px)";
     } else {
       $("#pin").css("animation","shake 0.5s");
     }
     return false;
-};
+  };
 })();
-
-
-// Webflow.push(function() {
-//   $('overlay'){
-//     e.preventDefault();
-// 	$('body').css('overflow', 'hidden');
-//   });
-//
-//   $('.overlay').onsubmit = function() {
-//     e.preventDefault();
-// 	$('body').css('overflow', 'auto');
-//   });
-// });
-
-// (function(){
-//   var $body = $(window.document.body);
-//   var a = document.getElementById('modal-key');
-//   $body.css('overflow', 'hidden');
-//   a.getElementsByTagName('form')[0].onsubmit = function(){
-//     if (this.answer.value === null) {
-//         $('body').css('overflow', 'hidden');
-//     } else if (this.answer.value === 'yrbk2018'){
-//       $body.css('overflow', 'hidden');
-//     }
-//     return false;
-//   }
-// });
-
-// Webflow.push(function() {
-//   $('overlay').click(function(e) {
-//     e.preventDefault();
-// 	$('body').css('overflow', 'hidden');
-//   });
-//
-//   $('.menu-class-name').click(function(e) {
-//     e.preventDefault();
-// 	$('body').css('overflow', 'auto');
-//   });
-// });
